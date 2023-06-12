3a8e2c576632d3105c177295c23e26f896bca20e
Jman45-6 <jeremy.cope7142@gmail.com>
2023-06-12 11:13:55 -0700

    added rotation prototype for the launchpad proto and got the mouse x and y to print to the console

--- gameCode.js
+++ gameCode.js
@@ -1,8 +1,12 @@
 
-const launchPad = document.getElementsByClassName("launchPad");
+const padImg = document.getElementById("padImg");
 
 window.onload = function() 
 { 
-
+    padImg.style.transform = "rotate(" + 45 +"deg)";
 }
 
+addEventListener("mousemove", (mouse) => {
+    console.log(mouse.screenX, mouse.screenY);
+    //need to make square point at mouse using trig
+});
