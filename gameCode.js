0bc18d89a00fd7bd6a585426240a316efe47f43a
Jeremy <117963339+jman45-9@users.noreply.github.com>
2023-06-12 16:47:45 -0700

    added function documentation

--- gameCode.js
+++ gameCode.js
@@ -6,6 +6,7 @@ window.onload = function()
     
 }
 
+//aims the cola can
 addEventListener("mousemove", (mouse) => {
     let mouseY = getYPos(mouse.clientY);
     let mouseX= getXPos(mouse.clientX);
@@ -15,11 +16,13 @@ addEventListener("mousemove", (mouse) => {
     padImg.style.transform = "rotate(" + rotationAngle +"deg)";
 });
 
+// gets the y position of the mouse on a cordinate plane centered at the bottom center of the window
 function getYPos(backwardsMouseY)
 {
     return window.innerHeight - backwardsMouseY;
 }
 
+// same as getYPos but with x cordinate
 function getXPos(absoluteMouseX) 
 {
     let screenMidpoint = (window.innerWidth / 2.0);
@@ -35,6 +38,7 @@ function getXPos(absoluteMouseX)
     return 0;
 }
 
+// converts radians to degrees
 function convertToDeg(input)
 {
     return input * (180 / Math.PI);
