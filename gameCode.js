70ff0a80761659ebb0dc494ea8b2c8aa710290b5
Jman45-6 <jeremy.cope7142@gmail.com>
2023-06-12 14:12:10 -0700

    mapped mouse position to the Cartesian Coordinate Plane so the trig works correctly.

--- gameCode.js
+++ gameCode.js
@@ -7,6 +7,28 @@ window.onload = function()
 }
 
 addEventListener("mousemove", (mouse) => {
-    console.log(mouse.screenX, mouse.screenY);
-    //need to make square point at mouse using trig
+    let mouseY = getYPos(mouse.clientY);
+    let mouseX= getXPos(mouse.clientX);
+    console.log(mouseX, mouseY);
+    //console.log(window.innerWidth, window.innerHeight);
+    //console.log(mouse.clientX, mouse.clientY);
 });
+
+function getYPos(backwardsMouseY)
+{
+    return window.innerHeight - backwardsMouseY;
+}
+
+function getXPos(absoluteMouseX) 
+{
+    let screenMidpoint = (window.innerWidth / 2.0);
+    if (screenMidpoint < absoluteMouseX)
+    {
+        let distanceFromEdge = window.innerWidth - absoluteMouseX;
+        return screenMidpoint - distanceFromEdge;
+    }
+    if (screenMidpoint > absoluteMouseX)
+    {
+        return -(screenMidpoint - absoluteMouseX)
+    }
+}
